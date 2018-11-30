d25ba742e485540e5f077a554790d513fd0f463c
Alexandr Hubenko <alexandrhubenko@Alexandrs-MacBook-Air.local>
2018-11-30 16:01:03 +0200

    fdsfsdf

--- dist/js/main.js
+++ dist/js/main.js
@@ -70,76 +70,57 @@ var menue = [
 
 
 let myMenue = document.querySelector(".myMenue");
-
-
-
-
-
-for (var i = 0; i < menue.length; i++) {
-  let menueItem = menue[i];
-  console.log('menueItem', menueItem);
-  const myUl = document.createElement("ul");
-  myUl.classList.add("parentUl");
-  myMenue.appendChild(myUl);
-  myUl.innerHTML = menueItem.title;
-  console.log(myUl);
-  console.log(menue[i])
-  var items = menue[i].items;
-  for (var j = 0; j < items.length; j++) {
-    let ulItem = items[j];
-    console.log("ulItem", ulItem);
-    const myLi = document.createElement("li");
-    myLi.classList.add("listParent");
-    myUl.appendChild(myLi);
-    myLink = document.createElement('a');
-    myLink.innerHTML = ulItem.title;
-    myLink.setAttribute('href',ulItem.link)
-    myLi.appendChild(myLink)
-    console.log(myLink);
-    console.log(myLi);
-    console.log(items[j]);
-    var items2 = items[j].items;
-    if (items2) {
-      for (var z = 0; z < items2.length; z++){
-        let lastItem = items2[z];
-        const deepUl = document.createElement("ul");
-        deepUl.classList.add('nestedUl');
-        myLi.appendChild(deepUl)
-        const deepestLi = document.createElement('li');
-        deepestLi.classList.add('deepestLi');
-        deepUl.appendChild(deepestLi);
-        myLink2 = document.createElement('a');
-        myLink2.innerHTML = lastItem.title;
-        myLink2.setAttribute('href', lastItem.link)
-        deepestLi.appendChild(myLink2)
-        console.log(items2[z])
-        console.log(lastItem.title);
-   }
+const createMenue = function () {
+  for (var i = 0; i < menue.length; i++) {
+    let menueItem = menue[i];
+    const myUl = document.createElement("ul");
+    myUl.classList.add("parentUl");
+    myMenue.appendChild(myUl);
+    myUl.innerHTML = menueItem.title;
+    var items = menue[i].items;
+    for (var j = 0; j < items.length; j++) {
+      let ulItem = items[j];
+      const myLi = document.createElement("li");
+      myLi.classList.add("listParent");
+      myUl.appendChild(myLi);
+      myLink = document.createElement('a');
+      myLink.innerHTML = ulItem.title;
+      myLink.setAttribute('href', ulItem.link)
+      myLi.appendChild(myLink)
+      const triangle = document.createElement("img");
+      triangle.setAttribute('src', '../img/triangle.jpg');
+      myLi.insertBefore(triangle, myLink);
+      var items2 = items[j].items;
+      if (items2) {
+        for (var z = 0; z < items2.length; z++) {
+          let lastItem = items2[z];
+          const deepUl = document.createElement("ul");
+          deepUl.classList.add('nestedUl');
+          myLi.appendChild(deepUl)
+          const deepestLi = document.createElement('li');
+          deepestLi.classList.add('deepestLi');
+          deepUl.appendChild(deepestLi);
+          myLink2 = document.createElement('a');
+          myLink2.innerHTML = lastItem.title;
+          myLink2.setAttribute('href', lastItem.link)
+          deepestLi.appendChild(myLink2)
+        }
+      }
+    }
   }
- }
 };
 
-const showItems = function(){
-var nestedUl = document.getElementsByClassName("nestedUl");
-for(let i=0; i<nestedUl.length; i++){
-  nestedUl[i].classList.remove('nestedUl');
- }
+const showItems = function () {
+  var nestedUl = document.getElementsByClassName("nestedUl");
+  for (let i = 0; i < nestedUl.length; i++) {
+    nestedUl[i].classList.remove('nestedUl');
+  }
 }
 
+createMenue();
 showItems();
 
 
-// const createTriangle =function (){
-// const triangle = document.createElement("img");
-// triangle.setAttribute('src', '../img/triangle.jpg');
-// console.log(triangle);
-// var listParent = getElementsByClassName("listParent");
-// if(li has children ul)
-// {
-//   for(i = 0; I<listParent.length; i++){
-//    listParent[i].appendChild(triangle)
-// }
-// }
 
 
 
